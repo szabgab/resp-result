28e271e51e43be175f9bf23c040dec33eda90e0d
Frozen String <964413011@qq.com>
2022-03-13 13:15:52 +0800

    :heavy_plus_sign: 添加为 actix-web 支持

--- src/resp_result/to_response.rs
+++ src/resp_result/to_response.rs
@@ -1,3 +1,4 @@
+use actix_web::HttpResponse;
 use axum::{
     body::{boxed, Full},
     response::IntoResponse,
@@ -8,50 +9,83 @@ use crate::resp_error::RespError;
 
 use super::RespResult;
 
-impl<T: serde::Serialize, E: RespError> IntoResponse for RespResult<T, E> {
+fn prepare_respond<T, E>(r: RespResult<T, E>) -> Result<(Vec<u8>, StatusCode), serde_json::Error>
+where
+    T: serde::Serialize,
+    E: RespError,
+{
+    let vec = serde_json::to_vec(&r);
+    let body = match vec {
+        Ok(body) => body,
+        Err(e) => {
+            #[cfg(feature = "log")]
+            logger::error!("RespResult 响应出现异常 : {}", e);
+            Err(e)?;
+            unreachable!()
+        }
+    };
+
+    let status = match r {
+        RespResult::Success(_) => {
+            #[cfg(feature = "log")]
+            logger::info!("RespResult 接管的 成功 响应",);
+            StatusCode::OK
+        }
+        RespResult::Err(ref e) => {
+            #[cfg(feature = "log")]
+            logger::info!(
+                "RespResult 接管的 异常 响应 | {} => {}",
+                std::any::type_name::<E>(),
+                e.description()
+            );
+            e.http_code()
+        }
+    };
+
+    #[cfg(feature = "log")]
+    logger::info!(
+        "RespResult 响应 准备构造 Status :{} BodySize: {}",
+        status,
+        body.len()
+    );
+    Ok((body, status))
+}
+
+static JSON_TYPE: &str = "application/json";
+
+#[cfg(feature = "for-axum")]
+impl<T, E> IntoResponse for RespResult<T, E>
+where
+    T: serde::Serialize,
+    E: RespError,
+{
     fn into_response(self) -> axum::response::Response {
-        let vec = serde_json::to_vec(&self);
-        let body = match vec {
-            Ok(body) => body,
-            Err(e) => {
-                #[cfg(feature = "log")]
-                logger::error!("RespResult 响应出现异常 : {}", e);
+        match prepare_respond(self) {
+            Ok((body, status)) => Response::builder()
+                .status(status)
+                .header(http::header::CONTENT_TYPE, JSON_TYPE)
+                .body(boxed(Full::from(body))),
+            Err(e) => Response::builder()
+                .status(StatusCode::INTERNAL_SERVER_ERROR)
+                .body(boxed(Full::from(e.to_string()))),
+        }
+        .expect("RespResult 构造响应时发生异常")
+    }
+}
 
-                return Response::builder()
-                    .status(StatusCode::INTERNAL_SERVER_ERROR)
-                    .body(boxed(Full::from(e.to_string())))
-                    .expect("RespResult 构造响应时发生异常")
-            }
-        };
+impl<T, E> actix_web::Responder for RespResult<T, E>
+where
+    T: serde::Serialize,
+    E: RespError,
+{
+    type Body = actix_web::body::BoxBody;
 
-        let status = match self {
-            RespResult::Success(_) => {
-                #[cfg(feature = "log")]
-                logger::info!("RespResult 接管的 成功 响应",);
-                StatusCode::OK
-            }
-            RespResult::Err(ref e) => {
-                #[cfg(feature = "log")]
-                logger::info!(
-                    "RespResult 接管的 异常 响应 | {} => {}",
-                    std::any::type_name::<E>(),
-                    e.description()
-                );
-                e.http_code()
-            }
-        };
-
-        #[cfg(feature = "log")]
-        logger::info!(
-            "RespResult 响应 准备构造 Status :{} BodySize: {}",
-            status,
-            body.len()
-        );
-
-        Response::builder()
-            .status(status)
-            .header(http::header::CONTENT_TYPE, "application/json")
-            .body(boxed(Full::from(body)))
-            .expect("RespResult 构造响应时发生异常")
+    fn respond_to(self, _req: &actix_web::HttpRequest) -> actix_web::HttpResponse<Self::Body> {
+        match prepare_respond(self) {
+            Ok((body, status)) => HttpResponse::build(status)
+                .content_type(JSON_TYPE)
+                .body(body),
+            Err(e) => HttpResponse::build(StatusCode::INTERNAL_SERVER_ERROR).body(e.to_string()),
+        }
     }
 }
