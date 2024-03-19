d7fa4ccefa2b9a0b6e5357f4971b84909abe3c2b
ForzenString <964413011@qq.com>
2024-03-19 14:11:40 +0800

    :sparkles: add reject on not support feature

--- code-gen/src/derive_resp_error/mod.rs
+++ code-gen/src/derive_resp_error/mod.rs
@@ -2,6 +2,7 @@ use darling::FromDeriveInput;
 use proc_macro2::TokenStream;
 use quote::quote;
 use syn::DeriveInput;
+use syn::spanned::Spanned;
 use crate::derive_resp_error::codegen::RespErrorCodeGen;
 use crate::derive_resp_error::input::RespErrorDeriveInput;
 
@@ -10,6 +11,9 @@ mod structure;
 mod codegen;
 
 pub fn gen_resp_error_derive(input:&DeriveInput)->syn::Result<TokenStream>{
+    if !input.generics.params.is_empty(){
+        Err(syn::Error::new(input.generics.span(),"Not support generic args"))?;
+    }
     let input=RespErrorDeriveInput::from_derive_input(input)?;
     let codegen:RespErrorCodeGen = input.try_into()?;
     Ok(quote!(#codegen))
