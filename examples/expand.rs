46d7880183dd1744d2f5c1756ec216771810a4be
ForzenString <964413011@qq.com>
2024-03-19 12:39:47 +0800

    :white_check_mark: update Example

--- examples/expand.rs
+++ examples/expand.rs
@@ -1,65 +1,34 @@
 use error::PlainError;
-
+use axum_resp_result::RespError;
 fn main() {
     let _ret = test((1, 2), String::new());
+    match _ret {
+        RespResult::Success(_) => {}
+        RespResult::Err(err) => {
+            println!("{:?},{:?}, {:?}", err.resp_message(), err.http_code(),err.log_message())
+        }
+    }
 }
 mod error {
-    use std::{borrow::Cow, num::ParseIntError};
+    use std::num::ParseIntError;
 
     use axum::extract::rejection::PathRejection;
     use axum_resp_result::RespError;
-    use http::StatusCode;
-
-    pub(super) struct PlainError {
-        pub(super) msg: String,
-        pub(super) _code: u32,
-    }
-
-    impl From<PathRejection> for PlainError {
-        fn from(err: PathRejection) -> Self {
-            Self::new(err.to_string(), 991)
-        }
-    }
-
-    impl From<ParseIntError> for PlainError {
-        fn from(value: ParseIntError) -> Self {
-            Self::new(value.to_string(), 1002)
-        }
-    }
-
-    impl PlainError {
-        pub(super) fn new(msg: String, _code: u32) -> Self {
-            Self { msg, _code }
-        }
-    }
-
-    impl RespError for PlainError {
-        fn log_message(&self) -> Cow<'_, str> {
-            format!("Plain Error Happened: {}", self.msg).into()
-        }
-
-        fn http_code(&self) -> http::StatusCode {
-            StatusCode::BAD_REQUEST
-        }
-
-        // type ExtraMessage = u32;
-
-        // fn extra_message(&self) -> Self::ExtraMessage {
-        //     self.code
-        // }
-
-        fn resp_message_default() -> Option<Cow<'static, str>> {
-            Some("Success".into())
-        }
-
-        // fn extra_message_default() -> Option<Self::ExtraMessage> {
-        //     Some(0)
-        // }
+    #[derive(Debug, thiserror::Error, RespError)]
+    pub(super) enum PlainError {
+        #[error("Parse Request Path Error: {0}")]
+        #[resp_result(err_msg = "Parse Request Path Error", err_code = 400)]
+        Path(#[from] PathRejection),
+        #[error("Parse Int Error: {0}")]
+        #[resp_result(err_msg = "Invalid Input Integer", err_code = "Bad Request")]
+        ParseInt(#[from] ParseIntError),
     }
 }
-use axum_resp_result::rresult;
+use axum_resp_result::{rresult, RespResult};
 #[rresult]
 fn test((a, b): (i32, i64), foo: String) -> Result<(), PlainError> {
     println!("{a},{b},{foo}");
+    let a = foo.parse::<i32>()?;
+    println!("{a:?}");
     Ok(())
 }
